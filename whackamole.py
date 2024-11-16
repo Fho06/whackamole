7fc0073dc96da29711d086c99add91975205e545
Fho06 <hochangfabian@gmail.com>
2024-11-16 14:27:56 -0500

    Update

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,11 @@
 import pygame
+import random
+
+def draw_grid(screen, cell_size, rows, cols, color):
+    for x in range(0, cols * cell_size, cell_size):
+        pygame.draw.line(screen, color, (x, 0), (x, rows * cell_size))
+    for y in range(0, rows * cell_size, cell_size):
+        pygame.draw.line(screen, color, (0, y), (cols * cell_size, y))
 
 
 def main():
@@ -10,11 +17,28 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+
+        #GRID COORDINATES
+        cell_size = 32
+        rows = 16
+        cols = 20
+
+        #MOLE COORDINATES
+        mole_x = 0
+        mole_y = 0
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                    mole_x = random.randrange(0, cols) * cell_size
+                    mole_y = random.randrange(0, rows) * cell_size
+
             screen.fill("light green")
+            draw_grid(screen, cell_size, rows, cols, "black")
+            screen.blit(mole_image, (mole_x,mole_y))
             pygame.display.flip()
             clock.tick(60)
     finally:
